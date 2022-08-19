84ca1c81dac0e7e32ee8a7e4a8981f297b4b7157
sulaiman01 <mohammadsulaiman01@gmail.com>
2022-08-19 16:07:33 +0530

    updated LeNet model codes

--- Image Classification Projects/LeNet/codes/model.py
+++ Image Classification Projects/LeNet/codes/model.py
@@ -4,5 +4,44 @@ import torch.nn as nn
 
 class LeNet(nn.Module):
     def __init__(self):
-        super.__init__()
-        self.layer1 = nn.Conv2d()
+        super().__init__()
+        self.conv1 = nn.Conv2d(1,6,(5,5))
+        self.subsample2 = nn.MaxPool2d((2,2))
+        self.conv3 = nn.Conv2d(6,16,(5,5))
+        self.subsample2 = nn.MaxPool2d((2,2))
+        self.conv5 = nn.Conv2d(16,1,(5,5))
+        self.fullyconnected6 = nn.Linear(120,84)
+        self.fullyconnected7 = nn.Linear(84,10)
+
+    def forward(self,x):
+        output = self.conv1(x)
+        output = nn.Tanh()(output)
+        output = self.subsample2(output)
+        output = self.conv2(output)
+        output = nn.Tanh()(output)
+        output = self.subsample2(output)
+        output = self.conv5(output)
+        output = nn.Tanh()(output)
+        output = torch.flatten(output)
+        output = self.fullyconnected6(output)
+        output = nn.Tanh()(output)
+        output = self.fullyconnected7(output)
+        output = nn.Softmax()(output)
+        return output
+    
+    def fit(self, data_loader, epoch, lr=None, optimizer=None, loss_function=None):
+        if optimizer is None:
+            if lr is None:
+                print('Need to provide "lr" argument if optimizer aurgument is not provided')
+            optimizer = torch.optim.SGD(self.parameters(),lr=lr)
+        if loss_function is None:
+            print('"loss" argument is missing')
+        for i in range(epoch):
+            output = self(data_loader)
+            loss = loss_function(output, )
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+if __name__ == "__main__":
+    model = LeNet()
